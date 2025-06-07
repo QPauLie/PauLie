7acd7a83cbe4303e9ae863fcb5052db50475e6f4
unknown <contact.mainakroy@gmail.com>
2025-06-07 14:36:30 +0530

    Use OTOC in tests

--- tests/test_second_moment.py
+++ tests/test_second_moment.py
@@ -3,8 +3,10 @@
 """
 import pytest
 import numpy as np
-from paulie.common.pauli_string_factory import get_pauli_string as p
+import networkx as nx
+from paulie.common.pauli_string_factory import get_pauli_string as p, get_identity
 from paulie.common.pauli_string_bitarray import PauliString
+from paulie.common.pauli_string_collection import PauliStringCollection
 from paulie.application.second_moment import second_moment
 
 def create_swap_matrix(n: int) -> np.ndarray:
@@ -21,38 +23,80 @@ def create_swap_matrix(n: int) -> np.ndarray:
         swap[i, j] = 1
     return swap
 
-@pytest.mark.parametrize("gens", [
-    ["I"], ["X"], ["Y"], ["Z"],
-    ["I", "X"], ["X", "Y"], ["Y", "Z"],
-    ["X", "Y", "Z"], ["I", "X", "Y", "Z"]
-])
-def test_frame_potential_from_second_order_twirl(gens: list[str]) -> None:
+def average_otoc(generators: PauliStringCollection, v: PauliString, w: PauliString) -> float:
     """
-    Reference: arxiv:2502.16404
+    Compute the OTOC using the commutator graph.
+    """
+    # Generate commutator graph
+    vertices, edges = generators.get_commutator_graph()
+    graph = nx.Graph()
+    graph.add_nodes_from(vertices)
+    graph.add_edges_from(edges)
+    # Get connected component of V
+    v_connected_component = nx.node_connected_component(graph, str(v))
+    # Count the number of elements t in the connected component of V
+    # that anticommute with W
+    anticommute_count = sum([not w | p(t) for t in v_connected_component])
+    return 1 - 2 * anticommute_count / len(v_connected_component)
 
-    There is a method get_frame_potential to get the frame
-    potential for any generating set. We can also compute
-    this quantity using second-order twirls (Appendix A).
+def otoc_from_twirl(generators: PauliStringCollection, v: PauliString, w: PauliString) -> float:
     """
-    n = len(gens[0])
-    gens = p(gens)
-    # Frame potential from commutator graph
-    fp_from_graph = gens.get_frame_potential()
-    # Frame potential from second-order twirls
-    fp_from_twirl = 0
-    i = PauliString(n=n)
-    swap = create_swap_matrix(n)
-    all_pauli_strings = list(i.gen_all_pauli_strings())
-    for a1 in all_pauli_strings:
-        for a2 in all_pauli_strings:
-            a1krona2 = a1 + a2
-            for b1 in all_pauli_strings:
-                for b2 in all_pauli_strings:
-                    b1kronb2 = p([(1, b1 + b2)])
-                    twirlb1b2 = second_moment(gens, b1kronb2)
-                    qq = a1krona2 @ twirlb1b2
-                    qqmat = qq.get_matrix()
-                    tr = np.trace(qqmat @ swap)
-                    fp_from_twirl += np.abs(tr) ** 2
-    fp_from_twirl /= 2 ** (4 * n)
-    assert fp_from_twirl == pytest.approx(fp_from_graph)
+    Compute the OTOC using Corollary 3 in Appendix A of arXiV:2502.16404.
+    """
+    vkronv = p([(1, str(v) + str(v))])
+    wkronw = p([(1, str(w) + str(w))])
+    wkronw_twirled = second_moment(generators, wkronw)
+    VVop = vkronv.get_matrix()
+    WWtop = wkronw_twirled.get_matrix()
+    swap = create_swap_matrix(len(v))
+    return np.trace(VVop @ WWtop @ swap) / 2 ** len(v)
+
+def incorrect_otoc_from_twirl(generators: PauliStringCollection, v: PauliString, w: PauliString) -> float:
+    """
+    Compute the OTOC using an incorrect SWAP matrix.
+    """
+    vkronv = p([(1, str(v) + str(v))])
+    wkronw = p([(1, str(w) + str(w))])
+    wkronw_twirled = second_moment(generators, wkronw)
+    VVop = vkronv.get_matrix()
+    WWtop = wkronw_twirled.get_matrix()
+    swap = np.eye(4 ** len(v))
+    return np.trace(VVop @ WWtop @ swap) / 2 ** len(v)
+
+generator_list = [
+    ["I"], ["X"], ["Y"], ["Z"],
+    ["ZI", "IZ", "XX"],
+    ["XI", "IX", "XY"],
+    ["XX", "YY", "ZZ"],
+]
+
+@pytest.mark.parametrize("generators", generator_list)
+def test_otoc_correct(generators: list[str]):
+    """
+    Test that the OTOC computed analytically and from the twirl match.
+    """
+    gens = p(generators)
+    i = get_identity(len(generators[0]))
+    analytical_values = []
+    computed_values = []
+    for v in i.gen_all_pauli_strings():
+        for w in i.gen_all_pauli_strings():
+            analytical_values.append(average_otoc(gens, v, w))
+            computed_values.append(otoc_from_twirl(gens, v, w))
+    assert all(a_val == pytest.approx(c_val) for a_val, c_val in zip(analytical_values, computed_values))
+
+@pytest.mark.parametrize("generators", generator_list)
+def test_otoc_incorrect(generators: list[str]):
+    """
+    Test that the OTOC computed analytically and from the twirl using
+    incorrect SWAP matrix do not match.
+    """
+    gens = p(generators)
+    i = get_identity(len(generators[0]))
+    analytical_values = []
+    computed_values = []
+    for v in i.gen_all_pauli_strings():
+        for w in i.gen_all_pauli_strings():
+            analytical_values.append(average_otoc(gens, v, w))
+            computed_values.append(incorrect_otoc_from_twirl(gens, v, w))
+    assert any(a_val != pytest.approx(c_val) for a_val, c_val in zip(analytical_values, computed_values))
