7b3207a50197ac623a7ef85a46910bed50433af5
oxana <shayaoxana@gmail.com>
2025-11-11 11:46:30 +0100

    docs optimal su 2 n generator

--- src/paulie/application/get_optimal_su2_n.py
+++ src/paulie/application/get_optimal_su2_n.py
@@ -1,22 +1,20 @@
-"""Get optimal generator set by fraction for su(2**n)"""
+"""Get generator set with optimal generation rate for su(2**n)"""
 import math
 from paulie.common.pauli_string_collection import PauliStringCollection
 
 
-def get_optimal_connections_su_2_n(generators:PauliStringCollection
-    )->tuple[int, int]:
-    """Get optimal number of connections in anticommutation graph for su(2**n)"""
+def get_optimal_connections_su_2_n(generators:PauliStringCollection) -> tuple[int, int]:
+    """ Get optimal number of connections in anticommutation graph for su(2**n) """
     fraction = 0.706
     ng = len(generators)
     total_pair = ng*(ng-1)/2
     if total_pair < 1:
-        return (-1, -1)
+        return -1
     n_pair = fraction * total_pair
-    n = math.floor(n_pair)
-    return n
+    n_pair = math.floor(n_pair)
+    return n_pair
 
-def get_optimal_su_2_n_generators(generators:PauliStringCollection) \
-        -> PauliStringCollection|None:
+def get_optimal_su_2_n_generators(generators:PauliStringCollection) -> PauliStringCollection|None:
     """ Get optimal generator set for su(2**n) """
     g = generators.copy().get_independents()
     n_pair = get_optimal_connections_su_2_n(g)
