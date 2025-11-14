7b83e7eb87a112dfc425fffbc6924615e3c37b54
Photopniv <tarin.teac@gmail.com>
2025-11-14 14:24:47 +0100

    Correct docstring

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -20,7 +20,7 @@ release = '0.0.1'
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
-#test
+
 extensions = [
 'sphinx.ext.autodoc',
 'sphinx.ext.autosummary',
