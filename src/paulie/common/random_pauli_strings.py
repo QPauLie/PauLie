63704a5eb733e2f0473634d8e40c213df04cc67c
Newtech66 <contact.mainakroy@gmail.com>
2026-03-12 03:46:04 +0530

    Complete random_pauli_strings

--- src/paulie/common/random_pauli_strings.py
+++ src/paulie/common/random_pauli_strings.py
@@ -1,6 +1,5 @@
 """
-A string representation of all Pauli strings of a given size. Needed to search for commutators.
-A bitarray is used for this as a simple way to sequentially increase Pauli strings.
+    Module to generate random Pauli strings.
 """
 from random import randint, choice
 
@@ -10,8 +9,8 @@ def get_random(n: int) -> str:
     Get random Pauli string of length n.
 
     Args:
-        n: Length of Pauli string.
-    Returns:
+        n (int): Length of Pauli string.
+    Returns: str
       Random Pauli string.
     """
     return ''.join([choice("IXYZ") for _ in range(n)])
@@ -21,12 +20,12 @@ def get_random_k_local(k: int, n: int) -> str:
     Get random k-local Pauli string of length n.
 
     Args:
-        k: Length of locality.
-        n: Length of Pauli string.
-    Returns:
+        k (int): Length of locality.
+        n (int): Length of Pauli string.
+    Returns: str
         Random k-local Pauli string of length n.
     Raises:
-         ValueError if k > n.
+         ValueError: If k > n.
     """
     if k > n:
         raise ValueError("Invalid args: first arg greater than second")
@@ -38,12 +37,12 @@ def get_random_k_local(k: int, n: int) -> str:
 
 def get_random_list(n: int, size: int) -> list[str]:
     """
-    Get random list of Pauli strings of length n.
+    Get list of random Pauli strings of length n.
 
     Args:
-         n: Length of Pauli string.
-         size: Size of array.
-    Returns:
+         n (int): Length of each Pauli string.
+         size (int): Size of list.
+    Returns: list[str]
         Random list of Pauli strings of length n.
     """
     return [get_random(n) for _ in range(size)]
