17404cf185acff96623f1ac4e8ebd36783918fe0
oxana <shayaoxana@gmail.com>
2026-04-21 20:55:09 +0200

    remove whitespaces

--- tests/test_dependency.py
+++ tests/test_dependency.py
@@ -8,7 +8,7 @@ from paulie.common.pauli_string_bitarray import PauliString
 def test_central_dependency_dropping():
     """
     Test that a generator dependent on the central vertex and other legs is dropped.
-    
+
     Bug: 'Incomplete Dependency Check'
     Previously, _dependency_check only performed Gaussian elimination on length-1 legs,
     ignoring the central_vertex. This allowed generators dependent on {central, leg1, ...}
@@ -24,21 +24,21 @@ def test_central_dependency_dropping():
     # All are connected to Central=XI.
     # L3 = Central @ L1 @ L2 (ignoring phases).
     gens = ["XI", "ZX", "IZ", "YY"]
-    
+
     cc = Canonicalizer()
     v_stack = [PauliString(pauli_str=g) for g in gens]
-    
+
     # Stack is LIFO. To ensure XI is Central, put it last so it's popped first.
     # v_stack[0] is XI
     ordered_stack = [v_stack[1], v_stack[2], v_stack[3], v_stack[0]]
-    
+
     cc.build_canonical_graph(ordered_stack)
-    
+
     # After fix, YY should be dropped because it is dependent on {XI, ZX, IZ}.
     # We expect 2 legs: [ZX] and [IZ].
     assert len(cc.legs) == 2
     assert cc.central_vertex == v_stack[0]
-    
+
     # Total vertices in canonical graph should be 3: XI, ZX, IZ.
     total_verts = 1 + sum(len(leg) for leg in cc.legs)
     assert total_verts == 3
@@ -51,13 +51,13 @@ def test_dla_dimension_with_redundant_generators():
     # Adding YY (which is XI*ZX*IZ) should not change the DLA.
     gens_independent = ["XI", "ZX", "IZ"]
     gens_redundant = ["XI", "ZX", "IZ", "YY"]
-    
+
     coll_indep = p(gens_independent)
     coll_redun = p(gens_redundant)
-    
+
     dim_indep = coll_indep.get_dla_dim()
     dim_redun = coll_redun.get_dla_dim()
-    
+
     assert dim_indep == 6
     assert dim_redun == 6
     assert coll_redun.get_algebra() == coll_indep.get_algebra()
--- tests/test_lightning_transfer.py
+++ tests/test_lightning_transfer.py
@@ -7,7 +7,7 @@ from paulie.common.pauli_string_bitarray import PauliString
 def test_lightning_transfer_from_internal_vertex():
     """
     Test that lightning can be transferred from a leg of length 2.
-    
+
     Bug: 'Limited Lightning Transfer'
     Previously, _transfer_lightning was restricted to legs of length exactly 2
     and only checked the first two vertices.
@@ -16,24 +16,24 @@ def test_lightning_transfer_from_internal_vertex():
     # Leg 1:   ZXI, IZX
     # v:       IIZ (Connects to IZX)
     gens = ["XII", "ZXI", "IZX", "IIZ"]
-    
+
     cc = Canonicalizer()
     v_stack = [PauliString(pauli_str=g) for g in gens]
-    
+
     # We want XII to be central.
     # Stack is LIFO. Pop XII first.
     # Then ZXI, IZX to build Leg 1.
     # Then IIZ to test transfer.
     ordered_stack = [v_stack[3], v_stack[2], v_stack[1], v_stack[0]]
-    
+
     cc.build_canonical_graph(ordered_stack)
-    
-    # In this case, ZXI and IZX @ XII = YYX (phase ignored) both anticommute 
-    # with the center XII, but commute with each other. 
+
+    # In this case, ZXI and IZX @ XII = YYX (phase ignored) both anticommute
+    # with the center XII, but commute with each other.
     # Thus they form two separate legs.
     # IIZ anticommutes with IZX (or its transformed version in the leg).
     # The fix ensures IIZ attaches to a leg correctly even if it's not the first leg.
-    
+
     # Output shows 2 legs: [ZXI] and [YYX, IIZ]
     assert len(cc.legs) == 2
     leg_lengths = sorted([len(leg) for leg in cc.legs])
@@ -48,24 +48,24 @@ def test_lightning_transfer_star_topology():
     # Leg 1:   ZXIII
     # Leg 2:   ZIXII, IIZXI, IIIZX
     # v:       IZIII (Connects to ZXIII)
-    
+
     gens = ["XIIII", "ZXIII", "ZIXII", "IIZXI", "IIIZX", "IZIII"]
-    
+
     cc = Canonicalizer()
     v_stack = [PauliString(pauli_str=g) for g in gens]
-    
+
     # Pop order (reverse): XIIII, then Leg 2, then Leg 1, then v.
     ordered_stack = [v_stack[5], v_stack[1], v_stack[4], v_stack[3], v_stack[2], v_stack[0]]
-    
+
     cc.build_canonical_graph(ordered_stack)
-    
+
     # The canonicalizer may reorganize legs during transfer/reduction.
     # The key is that it doesn't lose connections or erroneously create new legs
     # for generators that should be part of existing legs.
-    
+
     # If the bug were present, IZIII might have become a NEW leg (length 1),
     # resulting in more than 2 legs total if others didn't merge.
-    
+
     # Observations from failed run show [1, 4] leg lengths.
     # This means one leg has length 1, another length 4.
     # This happens if one generator is merged into another leg.
