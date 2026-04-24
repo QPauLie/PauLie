17db78945ce4b74a7d42125123de3302c09a66d3
Photopniv <tarin.teac@gmail.com>
2026-04-24 18:33:43 +0200

    Delete paulicompiler

--- src/paulie/__init__.py
+++ src/paulie/__init__.py
@@ -33,12 +33,6 @@ from .application.matrix_decomposition import (
     matrix_decomposition,
     matrix_decomposition_diagonal
 )
-from .application.pauli_compiler import (
-    compile_target,
-    OptimalPauliCompiler,
-    PauliCompilerConfig,
-    construct_universal_set,
-)
 
 # Classification
 from .classifier.classification import (
@@ -90,10 +84,6 @@ __all__ = [
     "get_pauli_weights",
     "matrix_decomposition",
     "matrix_decomposition_diagonal",
-    "compile_target",
-    "OptimalPauliCompiler",
-    "PauliCompilerConfig",
-    "construct_universal_set",
 
     # Classification
     "Morph",
